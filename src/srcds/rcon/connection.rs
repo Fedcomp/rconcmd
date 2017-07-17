43388167f536d50dedbc853ef1ad785c5099bb23
Fedcomp <aglergen@gmail.com>
2017-07-18 01:58:18 +0300

    [srcds:rcon:connection] Support for sending commands

--- src/srcds/rcon/connection.rs
+++ src/srcds/rcon/connection.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 use super::Packet;
-use super::PacketType::SERVERDATA_AUTH;
+use super::PacketType::{SERVERDATA_AUTH, SERVERDATA_EXECCOMMAND};
 use super::PacketDirection::OUTCOMING;
 
 const INVALID_RCON_ID: i32 = -1;
@@ -12,6 +12,7 @@ const INVALID_RCON_ID: i32 = -1;
 #[derive(Debug)]
 pub struct Connection {
     packet_id: i32,
+    connection: TcpStream
 }
 
 impl Connection {
@@ -23,7 +24,6 @@ impl Connection {
             net_type: SERVERDATA_AUTH,
             body: CString::new(rcon_password).unwrap(),
         };
-
         stream.write(&auth_packet.serialize())?;
 
         let auth_packet_response = Packet::read_from(&mut stream, OUTCOMING)?;
@@ -33,9 +33,23 @@ impl Connection {
                 "invalid rcon password",
             ))
         } else {
-            Ok(Connection { packet_id: 1 })
+            Ok(Connection {
+                packet_id: 1,
+                connection: stream
+            })
         }
+    }
 
+    pub fn send_command(&mut self, command: &str) -> Result<(), Error> {
+        let cmd = CString::new(command)?;
+        let command_packet = Packet {
+            id: 1,
+            net_type: SERVERDATA_EXECCOMMAND,
+            body: cmd
+        };
+
+        self.connection.write(&command_packet.serialize())?;
+        Ok(())
     }
 }
 
@@ -100,6 +114,63 @@ mod tests {
                 assert_eq!(e.description(), "invalid rcon password");
                 assert_eq!(e.kind(), ErrorKind::PermissionDenied);
             }
+        }
+    }
+
+    #[test]
+    fn test_send_command_success() {
+        const RCON_COMMAND: &str = "echo 123";
+        let (listener, hostname) = local_tcp_server();
+
+        let _t = thread::spawn(move || {
+            let mut connection = Connection::new(&hostname, VALID_RCON_PASSWORD).unwrap();
+            let _ = connection.send_command(RCON_COMMAND);
+        });
+
+        let (mut stream, _) = listener.accept().unwrap();
+
+        // Send success to connection
+        let success_auth_packet = Packet {
+            id: 0,
+            net_type: SERVERDATA_AUTH_RESPONSE,
+            body: CString::new("").unwrap(),
+        };
+        stream.write(&success_auth_packet.serialize()).unwrap();
+
+        // Drop auth packet
+        Packet::read_from(&mut stream, OUTCOMING).unwrap();
+
+        // Check if incoming command packet is correct
+        let command_packet = Packet::read_from(&mut stream, OUTCOMING).unwrap();
+
+        assert_eq!(command_packet.id, 1);
+        assert_eq!(command_packet.net_type, SERVERDATA_EXECCOMMAND);
+        assert_eq!(
+            command_packet.body.to_str().unwrap(),
+            RCON_COMMAND
+        );
+    }
+
+    #[test]
+    fn test_send_command_fail_server_closed_connection() {
+        const RCON_COMMAND: &str = "echo 123";
+        let (listener, hostname) = local_tcp_server();
+
+        let _t = thread::spawn(move || {
+            let (mut stream, _) = listener.accept().unwrap();
+
+            // Send success to connection
+            let success_auth_packet = Packet {
+                id: 0,
+                net_type: SERVERDATA_AUTH_RESPONSE,
+                body: CString::new("").unwrap(),
+            };
+            stream.write(&success_auth_packet.serialize()).unwrap();
+        });
+
+        let mut connection = Connection::new(&hostname, VALID_RCON_PASSWORD).unwrap();
+        if let Ok(_) = connection.send_command(RCON_COMMAND) {
+            panic!()
         }
     }
 }
