d849e184d9c0484ed0c798e63c61b2529d66eef9
Fedcomp <aglergen@gmail.com>
2017-07-18 00:19:50 +0300

    [srcds:rcon:connection] Error handling on invalid rcon

--- src/srcds/rcon/connection.rs
+++ src/srcds/rcon/connection.rs
@@ -1,9 +1,12 @@
 use std::ffi::CString;
-use std::io;
+use std::io::{Error, ErrorKind};
 use std::io::prelude::*;
 use std::net::TcpStream;
+
 use super::Packet;
-use super::PacketType::SERVERDATA_AUTH;
+use super::PacketType::{SERVERDATA_AUTH, SERVERDATA_AUTH_RESPONSE};
+
+const INVALID_RCON_ID: i32 = -1;
 
 #[derive(Debug)]
 pub struct Connection {
@@ -11,8 +14,8 @@ pub struct Connection {
 }
 
 impl Connection {
-    fn new(hostname: &str, rcon_password: &str) -> Result<Connection, io::Error> {
-        let mut stream = TcpStream::connect(hostname).unwrap();
+    fn new(hostname: &str, rcon_password: &str) -> Result<Connection, Error> {
+        let mut stream = TcpStream::connect(hostname)?;
 
         let auth_packet = Packet {
             id: 0,
@@ -20,11 +23,18 @@ impl Connection {
             body: CString::new(rcon_password).unwrap()
         };
 
-        stream.write(&auth_packet.serialize()).unwrap();
+        stream.write(&auth_packet.serialize())?;
 
-        Ok(Connection {
-            packet_id: 0
-        })
+        let auth_packet_response = Packet::read_from(&mut stream).unwrap();
+        println!("{:?}", auth_packet_response);
+        if auth_packet_response.id == INVALID_RCON_ID {
+            Err(Error::new(ErrorKind::PermissionDenied, "invalid rcon password"))
+        } else {
+            Ok(Connection {
+                packet_id: 1
+            })
+        }
+
     }
 }
 
@@ -33,8 +43,10 @@ mod tests {
     use super::*;
     use std::thread;
     use std::net::TcpListener;
+    use std::error::Error;
 
-    const RCON_PASSWORD: &str = "somespecialrconpassword";
+    const VALID_RCON_PASSWORD: &str = "somespecialrconpassword";
+    const INVALID_RCON_PASSWORD: &str = "somenonspecialrconpassword";
 
     fn local_tcp_server() -> (TcpListener, String) {
         let listener = TcpListener::bind("127.0.0.1:0").unwrap();
@@ -49,7 +61,7 @@ mod tests {
         let (listener, hostname) = local_tcp_server();
 
         let _t = thread::spawn(move || {
-            let _connection = Connection::new(&hostname, RCON_PASSWORD).unwrap();
+            let _connection = Connection::new(&hostname, VALID_RCON_PASSWORD).unwrap();
         });
 
         let (mut stream, _) = listener.accept().unwrap();
@@ -57,11 +69,34 @@ mod tests {
 
         assert_eq!(connection_packet.id, 0);
         assert_eq!(connection_packet.net_type, SERVERDATA_AUTH);
-        assert_eq!(connection_packet.body.to_str().unwrap(), RCON_PASSWORD);
+        assert_eq!(connection_packet.body.to_str().unwrap(), VALID_RCON_PASSWORD);
+
+        // _t.join();
     }
 
-    // #[test]
-    // fn test_new_fail_unreachable() {
-    //     let _connection_result = Connection::new("1.1.1.1:9999", RCON_PASSWORD);
-    // }
+    #[test]
+    fn test_new_fail_rcon() {
+        let (listener, hostname) = local_tcp_server();
+
+        let _t = thread::spawn(move || {
+            let (mut stream, _) = listener.accept().unwrap();
+
+            let failed_rcon_packet = Packet {
+                id: INVALID_RCON_ID,
+                net_type: SERVERDATA_AUTH_RESPONSE,
+                body: CString::new("").unwrap()
+            };
+            stream.write(&failed_rcon_packet.serialize()).unwrap();
+        });
+
+        match Connection::new(&hostname, INVALID_RCON_PASSWORD) {
+            Ok(_) => { panic!() },
+            Err(e) => {
+                println!("{:?}", e);
+
+                assert_eq!(e.description(), "invalid rcon password");
+                assert_eq!(e.kind(), ErrorKind::PermissionDenied);
+            }
+        }
+    }
 }
