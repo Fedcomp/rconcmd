a47b82000ff38d12ed8f236c64e81d65452264be
Fedcomp <aglergen@gmail.com>
2017-07-19 23:55:10 +0300

    Command line arguments

---
+++ README.md
@@ -0,0 +1,15 @@
+# rconcmd
+rcon console for srcds servers.
+Far from perfect, but it's working.
+
+### compilation
+
+```
+cargo build
+```
+
+### Usage
+
+```
+rconcmd 127.0.0.1:27015 %rcon_password%
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use std::io::Error;
 use std::io;
-use std::io::BufRead;
+use std::io::{Write, BufRead};
+
+extern crate clap;
+use clap::{Arg, App};
 
 extern crate rconcmd;
 use rconcmd::srcds::rcon::Connection;
 
 fn main() {
-    let mut connection = Connection::new("127.0.0.1:27015", "12345").unwrap();
+    let matches = App::new("rconcmd")
+                          .version("1.0")
+                          .author("Fedcomp")
+                          .about("Rcon console for srcds servers")
+                          .arg(Arg::with_name("hostname")
+                               .required(true)
+                               .takes_value(true))
+                          .arg(Arg::with_name("rcon")
+                               .required(true)
+                               .help("rcon_password of the server")
+                               .takes_value(true))
+                          .get_matches();
+
+    let hostname = matches.value_of("hostname").unwrap();
+    let rcon_password = matches.value_of("rcon").unwrap();
+
+    let mut connection = Connection::new(hostname, rcon_password).unwrap();
 
     loop {
         let command = read_from_stdin().unwrap();
@@ -16,9 +35,10 @@ fn main() {
 }
 
 fn read_from_stdin() -> Result<String, Error> {
+    print!("> ");
+    io::stdout().flush().unwrap();
     let mut input = String::new();
     let stdin = io::stdin();
-    print!("> ");
     stdin.lock().read_line(&mut input)?;
     Ok(input)
 }
