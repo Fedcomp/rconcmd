3225bf4242ffefb5988204008eb61c7fa87f46fa
Fedcomp <aglergen@gmail.com>
2018-08-13 01:57:41 +0300

    [tokio] Intermediate working console

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ extern crate clap;
 extern crate tokio;
 extern crate futures;
 extern crate rconcmd;
+extern crate tokio_io;
 extern crate tokio_game_protocols;
+
+mod utils;
+
+use utils::spawn_stdin_stream_unbounded;
 
 use std::ffi::CString;
 
@@ -13,6 +18,8 @@ use futures::{Future, Sink, Stream};
 
 use rconcmd::srcds::rcon::Connection;
 use tokio_game_protocols::srcds::rcon::{Packet, PacketType::*};
+
+use futures::sync::mpsc;
 
 fn main() {
     let matches = App::new("rconcmd")
@@ -31,33 +38,51 @@ fn main() {
     let hostname = matches.value_of("hostname").unwrap(); // unwrap because required
     let rcon_password = matches.value_of("rcon").unwrap(); // unwrap because required
 
-    let connection = Connection::connect(hostname, rcon_password).and_then(|connection| {
+    let connection = Connection::connect(hostname, rcon_password)
+    .and_then(|connection| {
         let proto = connection.proto;
         let packet_body = CString::new("echo 123").unwrap();
         let packet = Packet::new(0, SERVERDATA_EXECCOMMAND, packet_body);
+
         proto.send(packet)
     }).and_then(|proto| {
-        // let (proto_sink, proto_stream) = proto.split();
+        let (proto_sink, proto_stream) = proto.split();
+        let proto_sink = proto_sink.sink_map_err(|e| {
+            println!("Sink error: {:?}", e);
+        });
 
-        // tokio::spawn(proto_stream.for_each(|packet| {
-        //     if packet.net_type == SERVERDATA_RESPONSE_VALUE {
-        //         println!("{}", packet.body.into_string().unwrap());
-        //     }
-        //
-        //     Ok(())
-        // }).map_err(|_| {
-        //
-        // }));
+        let (tx, rx) = mpsc::unbounded();
+        tokio::spawn(
+            rx.forward(proto_sink).and_then(|_| Ok(())).map_err(|e| {
+                println!("rx forward error: {:?}", e);
+            })
+        );
 
-        // let stdin = spawn_stdin_stream_unbounded()
-        // .for_each(move |line| {
-        //     let command_packet = Packet::new(0, SERVERDATA_EXECCOMMAND, &line).unwrap();
-        //     let sending_future = proto_sink.send(command_packet).and_then(|_| Ok(())).map_err(|_| {});
-        //     // tokio::spawn(sending_future);
-        //     Ok(())
-        // });
+        // Stdin processing
+        tokio::spawn(
+            spawn_stdin_stream_unbounded().for_each(move |input| {
+                let packet_body = CString::new(input).unwrap();
+                let packet = Packet::new(0, SERVERDATA_EXECCOMMAND, packet_body);
 
-        Ok(())
+                tx.unbounded_send(packet).and_then(|_| Ok(())).map_err(|e| {
+                    println!("mspc send error: {:?}", e);
+                })
+            })
+        );
+
+        // Every incoming message
+        proto_stream.for_each(|incoming_packet| {
+            if incoming_packet.net_type != SERVERDATA_RESPONSE_VALUE {
+                return Ok(())
+            }
+
+            match incoming_packet.body.into_string() {
+                Ok(s) => print!("{}", s),
+                Err(e) => print!("{:?}", e.into_cstring())
+            };
+
+            Ok(())
+        })
     }).map_err(|err| {
         println!("err = {:?}", err);
     });
---
+++ src/utils/mod.rs
@@ -0,0 +1,32 @@
+use std::thread;
+use std::io;
+use futures::sync::mpsc::{channel, unbounded, Receiver, SendError, UnboundedReceiver};
+use tokio::prelude::stream::iter;
+use futures::Sink;
+use std::io::Read;
+use futures::Stream;
+use futures::Future;
+use std::io::BufRead;
+
+#[derive(Debug)]
+enum Error {
+    Stdin(io::Error),
+    Channel(SendError<String>),
+}
+
+pub fn spawn_stdin_stream_unbounded() -> UnboundedReceiver<String> {
+    let (channel_sink, channel_stream) = unbounded();
+    let stdin_sink = channel_sink.sink_map_err(Error::Channel);
+
+    thread::spawn(move || {
+        let stdin = io::stdin();
+        let stdin_lock = stdin.lock();
+        iter(stdin_lock.lines())
+            .map_err(Error::Stdin)
+            .forward(stdin_sink)
+            .wait()
+            .unwrap();
+    });
+
+    channel_stream
+}
