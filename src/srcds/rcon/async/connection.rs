2111d60318862fd870328b77baf987ecb9822c77
Fedcomp <aglergen@gmail.com>
2018-08-12 20:41:51 +0300

    [tokio] Unbox future

--- src/srcds/rcon/async/connection.rs
+++ src/srcds/rcon/async/connection.rs
@@ -16,31 +16,29 @@ pub struct Connection {
 const INVALID_RCON_ID: i32 = -1;
 
 impl Connection {
-    pub fn connect(host: &str, rcon_password: &str) -> Box<Future<Item = Connection, Error = Error> + Send> {
+    pub fn connect(host: &str, rcon_password: &str) -> impl Future<Item = Connection, Error = Error> {
         let auth_packet = Packet::new(0, SERVERDATA_AUTH, rcon_password).unwrap();
 
-        Box::new(
-            TcpStream::connect(&host[..])
-                .and_then(move |tcp| {
-                    let proto = tcp.framed(RconCodec::new());
-                    proto.send(auth_packet)
-                })
-                // Generate request for first response packet
-                .and_then(|proto| proto.into_future().map_err(|(e, _)| e) )
-                // skip SERVERDATA_RESPONSE_VALUE
-                // TODO: Handle stream is over
-                .and_then(|(_, proto)| proto.into_future().map_err(|(e, _)| e))
-                .and_then(|(auth_response, proto)| {
-                    if let Some(auth_response_packet) = auth_response {
-                        if auth_response_packet.id == INVALID_RCON_ID {
-                            Err(Error::new(ErrorKind::Other, "Invalid RCON password"))
-                        } else {
-                            Ok(Connection { proto: proto })
-                        }
+        TcpStream::connect(&host[..])
+            .and_then(move |tcp| {
+                let proto = tcp.framed(RconCodec::new());
+                proto.send(auth_packet)
+            })
+            // Generate request for first response packet
+            .and_then(|proto| proto.into_future().map_err(|(e, _)| e) )
+            // skip SERVERDATA_RESPONSE_VALUE
+            // TODO: Handle stream is over
+            .and_then(|(_, proto)| proto.into_future().map_err(|(e, _)| e))
+            .and_then(|(auth_response, proto)| {
+                if let Some(auth_response_packet) = auth_response {
+                    if auth_response_packet.id == INVALID_RCON_ID {
+                        Err(Error::new(ErrorKind::Other, "Invalid RCON password"))
                     } else {
-                        Err(Error::new(ErrorKind::Other, "Connection closed (?)"))
+                        Ok(Connection { proto: proto })
                     }
-                }),
-        )
+                } else {
+                    Err(Error::new(ErrorKind::Other, "Connection closed (?)"))
+                }
+            })
     }
 }
